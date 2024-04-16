98ddea09b1896bf6248fd727b9bca96cb226e6aa
Mia <143561609+MiaKoppen1@users.noreply.github.com>
2024-04-16 06:32:37 -0700

    Update SceneFrame.java
    changes made to make it look better

--- SceneFrame.java
+++ SceneFrame.java
@@ -1,78 +1,33 @@
 
 import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-/**
- * SceneFrame extends JFrame and sets up the scene component and interaction buttons.
- */
 public class SceneFrame extends JFrame 
     {
     public SceneFrame() 
         {
         setTitle("Scene Editor");
-        setSize(600, 300);  // Default size of the frame
+        setSize(600, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // Create the scene component where shapes will be drawn and interacted with
         SceneComponent scene = new SceneComponent();
 
-        // Create buttons and define their actions
         JButton houseButton = new JButton("House");
-
-        houseButton.addActionListener(new ActionListener() 
-            {
-            public void actionPerformed(ActionEvent event) 
-                {
-                scene.add(new HouseShape(20, 20, 50));
-                }
-            });
-
         JButton carButton = new JButton("Car");
+        JButton truckButton = new JButton("Truck");
+        JButton stopSignButton = new JButton("Stop Sign");
+        JButton removeButton = new JButton("Remove");
 
-        carButton.addActionListener(new ActionListener() 
-            {
-            public void actionPerformed(ActionEvent event) 
-                {
-                scene.add(new CarShape(20, 20, 50));
-                }
-            });
+         // Set up action listeners
+        houseButton.addActionListener(e -> scene.add(new HouseShape(20, 20, 50)));
+        carButton.addActionListener(e -> scene.add(new CarShape(20, 20, 20)));
+        truckButton.addActionListener(e -> scene.add(new TruckShape(20, 20, 20)));
+        stopSignButton.addActionListener(e -> scene.add(new StopSignShape(20, 20, 20)));
+        removeButton.addActionListener(e -> scene.removeSelected());
 
-            JButton stopSignButton = new JButton("Stop Sign");
-
-            carButton.addActionListener(new ActionListener() 
-                {
-                public void actionPerformed(ActionEvent event) 
-                    {
-                    scene.add(new StopSignShape(20, 20, 50));
-                    }
-                });
-
-            JButton truckButton = new JButton("Truck");
-
-            carButton.addActionListener(new ActionListener() 
-                {
-                public void actionPerformed(ActionEvent event) 
-                    {
-                    scene.add(new TruckShape(20, 20, 50));
-                    }
-                });
-
-            JButton removeButton = new JButton("Remove");
-            removeButton.addActionListener(new ActionListener() 
-                {
-                public void actionPerformed(ActionEvent event) 
-                    {
-                    System.out.println("Remove button clicked");
-                    scene.removeSelected();
-                    }
-                });
-
-        // Panel to hold the buttons
         JPanel buttonsPanel = new JPanel();
         buttonsPanel.add(houseButton);
         buttonsPanel.add(carButton);
@@ -80,7 +35,6 @@ public class SceneFrame extends JFrame
         buttonsPanel.add(stopSignButton);
         buttonsPanel.add(removeButton);
 
-        // Add the scene and buttons panel to the frame
         add(scene, BorderLayout.CENTER);
         add(buttonsPanel, BorderLayout.NORTH);
         }
