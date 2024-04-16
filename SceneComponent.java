f5d28f9e4d3a834bcf6467a60a78999892fec3e8
Mia <143561609+MiaKoppen1@users.noreply.github.com>
2024-04-16 06:31:50 -0700

    Update SceneComponent.java
    changes made to make it look better

--- SceneComponent.java
+++ SceneComponent.java
@@ -1,102 +1,109 @@
-
-
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.ArrayList;
+import java.util.ArrayList;  // Import statement for ArrayList
 
 import javax.swing.JComponent;
 
-/**
- * SceneComponent manages a list of SceneShapes and handles drawing and user interactions.
- */
 public class SceneComponent extends JComponent 
     {
     private ArrayList<SceneShape> shapes;
     private SceneShape selectedShape;
+    private boolean dragging;
+    private int prevX, prevY;
 
     public SceneComponent() 
         {
         shapes = new ArrayList<>();
         selectedShape = null;
+        dragging = false;
 
-        setBackground(Color.WHITE); // Set a different color to confirm component visibility
-        setOpaque(true); // Make sure the component is opaque to show background
-    // Other initialization code...
-        
-        // Mouse listener to handle shape selection
+        setBackground(Color.WHITE);
+        setOpaque(true);
+
         MouseAdapter mouseAdapter = new MouseAdapter() 
             {
             @Override
-            public void mouseClicked(MouseEvent e) 
+            public void mousePressed(MouseEvent e) 
                 {
-                boolean found = false; // flag to track if a shape is found
+                SceneShape prevSelected = selectedShape;
+                selectedShape = null;
                 for (int i = shapes.size() - 1; i >= 0; i--) 
                     {
                     SceneShape shape = shapes.get(i);
                     if (shape.getBounds().contains(e.getPoint())) 
                         {
                         selectedShape = shape;
-                        found = true;
+                        if (selectedShape != prevSelected) 
+                            {
+                            repaint();
+                            }
+                        prevX = e.getX();
+                        prevY = e.getY();
+                        dragging = true;
                         break;
                         }
                     }
-                if (!found) 
+                if (selectedShape == null) 
                     {
-                    selectedShape = null;
-                    System.out.println("No shape found at click location");  // Debugging statement
-                    }
                     repaint();
+                    }
+                }
+
+            @Override
+            public void mouseDragged(MouseEvent e) 
+                {
+                if (dragging && selectedShape != null) 
+                    {
+                    int deltaX = e.getX() - prevX;
+                    int deltaY = e.getY() - prevY;
+                    selectedShape.setPosition(selectedShape.getX() + deltaX, selectedShape.getY() + deltaY);
+                    prevX = e.getX();
+                    prevY = e.getY();
+                    repaint();
+                    }
+                }
+
+            @Override
+            public void mouseReleased(MouseEvent e) 
+                {
+                dragging = false;
                 }
             };
-             // Add the mouse listener to this component
-        this.addMouseListener(mouseAdapter);
+        addMouseListener(mouseAdapter);
+        addMouseMotionListener(mouseAdapter);
         }
-           /**
-     * Adds a new shape to the component.
-     * @param shape the shape to be added
-     */
+
+    @Override
+    protected void paintComponent(Graphics g) 
+        {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        for (SceneShape shape : shapes) 
+            {
+            shape.draw(g2);
+            if (shape.equals(selectedShape)) 
+                {
+                shape.drawSelection(g2);
+                }
+            }
+        }
+
     public void add(SceneShape shape) 
         {
         shapes.add(shape);
         repaint();
         }
 
-
-// Removes the shape from the component.
-
-public void removeSelected() 
-    {
-    if (selectedShape != null) 
+    public void removeSelected() 
         {
-        System.out.println("Removing selected shape");
-        shapes.remove(selectedShape);
-        selectedShape = null;
-        repaint();
-        } 
-        else 
+        if (selectedShape != null) 
             {
-            System.out.println("No shape selected to remove");
-            }
-        }
-
-@Override
-protected void paintComponent(Graphics g) 
-    {
-    super.paintComponent(g);
-    Graphics2D g2 = (Graphics2D) g;
-    System.out.println("Painting component with " + shapes.size() + " shapes");
-    for (SceneShape shape : shapes) 
-        {
-        shape.draw(g2);
-        if (shape.equals(selectedShape)) 
-            {
-            System.out.println("Drawing selection for a shape");
-            shape.drawSelection(g2);
+            shapes.remove(selectedShape);
+            selectedShape = null;
+            repaint();
             }
         }
     }
-}
-    
